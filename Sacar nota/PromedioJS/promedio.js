016b97e8ed03067ecf0a50670db08f1ae2bbde20
dannydf123 <52668676+dannydf123@users.noreply.github.com>
2019-07-19 11:51:03 -0500

    Add files via upload

---
+++ Sacar nota/PromedioJS/promedio.js
@@ -0,0 +1,27 @@
+
+
+nota1 = prompt ("ingrese su nota #1: ")
+nota2 = prompt ("ingrese su nota #1: ")
+nota3 = prompt ("ingrese su nota #1: ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById("nota1").innerHTML = nota1
+document.getElementById("nota2").innerHTML = nota2
+document.getElementById("nota3").innerHTML = nota3
+document.getElementById("notaFinal").innerHTML = notaFinal
+document.getElementById("gano").innerHTML = gano
+document.getElementById("pedio").innerHTML = perdio
